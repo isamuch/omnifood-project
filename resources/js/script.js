704102e8552735c491fdaa41e853943df301d422
Samuch Juntha-anukul <59326638+isamuch@users.noreply.github.com>
2020-04-06 02:27:06 +0700

    Add animate

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,6 +11,52 @@ $(document).ready(function () {
       offset: '60px',
     }
   );
+
+  // Animate
+  $('.js--wp-1').waypoint(
+    function (direction) {
+      if (direction == 'down') {
+        $('.js--wp-1').addClass('animated fadeIn');
+      }
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-2').waypoint(
+    function (direction) {
+      if (direction == 'down') {
+        $('.js--wp-2').addClass('animated fadeInUp');
+      }
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-3').waypoint(
+    function (direction) {
+      if (direction == 'down') {
+        $('.js--wp-3').addClass('animated fadeIn');
+      }
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-4').waypoint(
+    function (direction) {
+      if (direction == 'down') {
+        $('.js--wp-4').addClass('animated pulse');
+      }
+    },
+    {
+      offset: '50%',
+    }
+  );
+
   //   Mobile Navigation
 
   $('.js--nav-icon').click(function () {
